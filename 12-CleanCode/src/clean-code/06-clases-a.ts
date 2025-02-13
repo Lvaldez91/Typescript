a3ca6e848eb55e971fad4b566575f7bc2c36332c
Lucero Valdez <lvaldezd.isc91@gmail.com>
2025-02-12 22:50:17 -0600

    Add changes

--- 12-CleanCode/src/clean-code/06-clases-a.ts
+++ 12-CleanCode/src/clean-code/06-clases-a.ts
@@ -4,17 +4,27 @@ type Gender = 'M'|'F';
 // Clase anonima
 (()=>{
     class Person{
-        public name: string;
-        public gender: string;
-        public brithdate: Date;
+        // public name: string;
+        // public gender: string;
+        // public brithdate: Date;
 
-        constructor(name:string, gender:string, brithdate:Date) {
-            this.name = name;
-            this.gender = gender;
-            this.brithdate = brithdate;
+        constructor(public name:string, public gender:string, public brithdate:Date) {
+            // this.name = name;
+            // this.gender = gender;
+            // this.brithdate = brithdate;
         }
     }
+    // Class that extends to Person object
+    class User extends Person {
+        constructor(public email:string, public role:string, private lastAccess: Date, name:string, gender:Gender, brithDate:Date) {
+            super(name, gender, brithDate);
+        }
+
+        checkCredentials(){ return true; }
+    }
+
 
     const newPerson= new Person('Lucero','F',new Date('1991-07-19'));
     console.log(newPerson);
+
 })();
